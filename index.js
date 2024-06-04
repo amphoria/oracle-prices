c64a35b89097216e5e9e01851bb22f5abf8166ff
amphoria <34939932+amphoria@users.noreply.github.com>
2024-06-04 18:20:09 +0100

    Added re.al reETH and USTB prices ratios

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const osethPrice = document.getElementById('oseth-price')
 const oethPrice = document.getElementById('oeth-price')
 const primeEthPriceRatio = document.getElementById('prime-eth-ratio')
 const primeEthPrice = document.getElementById('prime-eth-price')
+const reETHPriceRatio = document.getElementById('reeth-price-ratio')
+const ustbPriceRatio = document.getElementById('ustb-price-ratio')
 
 // DIA Oracle for OETH/ETH
 const diaAddress = "0xAB9e20E8aB03C4F9476531B3ba3A0fc522Cd0FAa"
@@ -23,17 +25,34 @@ const primeETHOracleABI =
     "function primeETHPrice() view returns (uint256)"
 ]
 
+// reETH price
+const reETHAddress = "0xC0Cc5eA00cAe0894B441E3B5a3Bb57aa92F15421"
+const reETHABI = 
+[
+    "function tokenPrice() view returns (uint256)"
+]
+
+// USTB Price
+const ustbAddress = "0x83feDBc0B85c6e29B589aA6BdefB1Cc581935ECD"
+const ustbABI =
+[
+    "function rebaseIndex() view returns (uint256)"
+]
+
 // Ethers provider objects
 // const ethProvider = 
 //     new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY)
 const ethProvider = 
     new ethers.JsonRpcProvider("https://mainnet.infura.io/v3/ca1b1cda8d6940e6af90ec7b1b8cf84d")
+const realProvider = new ethers.JsonRpcProvider("https://real.drpc.org")
 
 // Ethers contract objects
 const diaContract = new ethers.Contract(diaAddress, diaABI, ethProvider)
 const primeETHOracleContract = new ethers.Contract(primeETHOracleAddress, 
                                                    primeETHOracleABI, 
                                                    ethProvider)
+const reETHContract = new ethers.Contract(reETHAddress, reETHABI, ethProvider)
+const ustbContract = new ethers.Contract(ustbAddress, ustbABI, realProvider)
 
 async function getEthPrice() {
     const redstonePrice = await redstone.getPrice("ETH")
@@ -55,6 +74,16 @@ async function getPrimeEthRatio() {
     return primeEthRatio
 }
 
+async function getREETHPriceRatio() {
+    const reETHPriceRatio = await reETHContract.tokenPrice()
+    return reETHPriceRatio
+}
+
+async function getUSTBPriceRatio() {
+    const ustbPriceRatio = await ustbContract.rebaseIndex()
+    return ustbPriceRatio
+}
+
 function round(num, digits) {
     return Math.round(num * 10**digits) / 10**digits
 }
@@ -68,6 +97,11 @@ async function main() {
     const primeEthRatio = round(Number(ethers.formatUnits(await getPrimeEthRatio(), 18)), 8)
     primeEthPriceRatio.textContent = primeEthRatio
     primeEthPrice.textContent = round(ethPriceUsd * primeEthRatio, 3)
+    const reEthRatio = round(Number(ethers.formatUnits(await getREETHPriceRatio(), 18)), 8)
+    reETHPriceRatio.textContent = reEthRatio
+    const ustbRatio = ethers.formatUnits(await getUSTBPriceRatio())
+    ustbPriceRatio.textContent = ustbRatio
+
 }
 
 main()
